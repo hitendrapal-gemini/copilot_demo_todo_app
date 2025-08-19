2e6ee01b77b464f6910ecb70d3761189bf4f0e99
nishant garg <nishant.garg@geminisolutions.com>
2025-08-19 17:32:09 +0530

    modifying config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'bhaiyokabhaipraveshbhai')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'abcdefgfghijklmnop')
     AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
-    EMAIL = "programmerpravesh@gmail.com"
+    EMAIL = "alicebob@gmail.com"
